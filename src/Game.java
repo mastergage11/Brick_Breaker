07422f36f267a9d0493c707f23c93e47b4f57949
mastergage11 <master.gage11@yahoo.com>
2020-02-10 12:42:35 -0500

    need to work on brick collision and/or player movement

--- src/Game.java
+++ src/Game.java
@@ -14,6 +14,7 @@ public class Game extends JFrame implements KeyListener {
 
         board = new Board(this);
         add(board);
+        addKeyListener(this);
         pack();
 
         board.setup();
